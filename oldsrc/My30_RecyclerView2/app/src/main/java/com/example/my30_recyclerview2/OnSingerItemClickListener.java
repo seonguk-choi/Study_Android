b0f71f0375fd1e7a38e864cbf9733d80d6253d16
seongukchoi <cabin2351@gmail.com>
2021-12-29 20:13:38 +0900

    12/29 20:13 choi commit

---
+++ oldsrc/My30_RecyclerView2/app/src/main/java/com/example/my30_recyclerview2/OnSingerItemClickListener.java
@@ -0,0 +1,8 @@
+package com.example.my30_recyclerview2;
+
+import android.view.View;
+
+public interface OnSingerItemClickListener {
+    public void onItemClick(SingerAdapter.ViewHoler holderm,
+                            View view, int position);
+}
